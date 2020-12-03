5a084771c515d2590019e0f6e6ee216913e6067d
baduserrr <badu.siahaan@gmail.com>
2020-12-03 07:41:09 +0700

    Update
    -Menambahkan bubble ke setiap page
    -Membuat active header

--- header.js
+++ header.js
@@ -1,18 +1,28 @@
 document.write(`
-  <div class="row justify-content-center fixed-top">
+  <div class="row justify-content-center fixed-top" id="header">
     <div class="col-8 navfix">
       <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <a class="navbar-brand pl-2 pt-0 pr-0" href="index.html">
           <img src="imgs/Logo.png" width="40" height="40" class="d-inline-block align-top" alt="" loading="lazy">
         </a>
         <div class="collapse navbar-collapse" id="Navbar">
-          <div class="navbar-nav">
+        <ul class="navbar-nav">
+          <li class="nav-item index">
             <a class="nav-link" href="index.html">ホーム <span class="sr-only">(current)</span></a>
-            <a class="nav-link" href="origami.html">折り紙</a>
-            <a class="nav-link" href="kirigami.html">切り紙</a>
-            <a class="nav-link" href="papercraft.html">ペーパークラフト</a>
-            <a class="nav-link" href="cardboard.html">段ボール</a>
-          </div>
+          </li>
+          <li class="nav-item origami">
+            <a class="nav-link origami" href="origami.html">折り紙</a>
+          </li>
+          <li class="nav-item kirigami">
+            <a class="nav-link kirigami" href="kirigami.html">切り紙</a>
+          </li>
+          <li class="nav-item papercraft">
+            <a class="nav-link papercraft" href="papercraft.html">ペーパークラフト</a>
+          </li>
+          <li class="nav-item cardboard">
+            <a class="nav-link cardboard" href="cardboard.html">段ボール</a>
+          </li>
+        </ul>
         </div>
       </nav>
     </div>
