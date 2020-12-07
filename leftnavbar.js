5eebea76676baeeb3e6e51623c06369890b93ead
baduserrr <badu.siahaan@gmail.com>
2020-12-07 14:15:26 +0700

    Trying to add mobile support

--- leftnavbar.js
+++ leftnavbar.js
@@ -1,10 +1,6 @@
 document.write(`
-  <ul class="nav flex-column nav-pills pt-3 pl-3 pb-3" id="leftnavbar">
-    <li class="nav-item">
-      <a class="nav-link technique" href="technique.html">技法</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link tande" href="tande.html">器材</a>
-    </li>
+  <ul class="nav nav-pills flex-column flex-md-row pt-3 pl-3 pb-3" id="leftnavbar">
+    <a class="nav-link technique" href="technique.html">技法</a>
+    <a class="nav-link tande" href="tande.html">器材</a>
   </ul>
 `);
