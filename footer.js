06a7378fba9ca95e749060880423fc93d9e54427
baduserrr <badu.siahaan@gmail.com>
2020-12-28 17:25:56 +0700

    minor changes

--- footer.js
+++ footer.js
@@ -2,7 +2,7 @@ document.write(`
   <div class="col-lg-8 bg-dark shadow-xl border border-dark border-top-0 rounded-bottom">
     <button id="btnup" onclick="topFunction()" type="button" class="btn btn-secondary btnup"><img src=imgs/chevron-up-solid.png class="btnupimg"></button>
     <div class="text-light pt-2">
-      <h2 style="text-align:center;">Powered by Bootsrap</h2>
+      <h5 style="text-align:center;">Powered by Bootsrap</h5>
     </div>
   </div>
 `);
